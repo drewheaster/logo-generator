ad445d4474df58a3c875aa85eeaf4c12965fbfc5
Drew Easter <drewheaster@gmail.com>
2024-08-12 01:26:33 -0500

    added questions/logo creation functionality

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Square = require('./lib/createSquare');
+const Circle = require('./lib/createCircle');
 const Triangle = require('./lib/createTriangle');
 
 const createLogo = function() {
@@ -23,8 +25,36 @@ const createLogo = function() {
                 name: 'text'
             }
         ]).then((answers) => {
-            if(answers.shape === 'Square'){
-                console.log('Chose square')
+            if(answers.shape === 'Square') {
+                const square = new Square(answers.color, answers.text);
+
+                fs.writeFile('square.svg', square.shape, (err) => {
+                    if(err) {
+                        console.error(err);
+                    } else {
+                        console.log('Successfully created square logo!');
+                    };
+                });
+            } else if(answers.shape === 'Circle') {
+                const circle = new Circle(answers.color, answers.text);
+
+                fs.writeFile('circle.svg', circle.shape, (err) => {
+                    if(err) {
+                        console.error(err);
+                    } else {
+                        console.log('Successfully created circle logo!');
+                    };
+                });
+            } else if (answers.shape === 'Triangle') {
+                const triangle = new Triangle(answers.color, answers.text);
+
+                fs.writeFile('circle.svg', triangle.shape, (err) => {
+                    if(err) {
+                        console.error(err);
+                    } else {
+                        console.log('Successfully created triangle logo!');
+                    };
+                });
             }
         });
 
